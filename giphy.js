4dd4de1ee25e198cb45a3e5a1802f1303d6c8c47
Kevin Wong <kevinwong@pursuit.org>
2019-12-17 20:01:02 -0500

    add populateSelect() and giphySearch() functions

--- giphy.js
+++ giphy.js
@@ -1,9 +1,35 @@
-// WARMUP:
-// 1. I want you to create a directory(folder) with a JavaScript file, a HTML file, and CSS file.
-// 2. The <body> tag should contain a <form> tag, and a <div>tag with a class of “content”
-// 3. the <form> tag should contain an <input> tag with type=text, a select tag with a disabled <option> tag and another input tag with the type=submit
-// 2. Create an account on giphy https://developers.giphy.com/?gclid=EAIaIQobChMI9LC-1eu95gIVDYiGCh0OPAnvEAAYASAAEgL0E_D_BwE
-// developers.giphy.comdevelopers.giphy.com
-// GIPHY Developers
-// The GIPHY Developer Portal is how you integrate the world's best GIF API into your business, app or project.
+let key = "EuNi39MeeBPzJPWLqTWVYXxSFFgecZqB"
 
+document.addEventListener("DOMContentLoaded", () => {
+    let form = document.querySelector("form");
+    let input = document.querySelector("#userInput");
+    let select = document.querySelector("select");
+    let submit = document.querySelector("#submit");
+    let content = document.querySelector(".content");
+
+    let h1 = document.createElement("h1");
+
+    form.addEventListener('submit', () => {
+        giphySearch(search, limit);
+    })
+
+    const giphySearch = async (userInput, userLimit) => {
+        try{
+            let res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userInput}&limit=${userLimit}`)
+            console.log(res);
+        } catch (err){
+            console.log(err);
+        }
+    }
+
+    const populateSelect = () => {
+        for(let i = 1; i <= 25; i ++){
+            let option = document.createElement('option');
+            option.value = i;
+            option.innerText = i;
+            select.appendChild(option);
+        }
+    }
+
+    populateSelect();
+})
