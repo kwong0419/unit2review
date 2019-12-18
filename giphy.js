ebce00607433171ede1f926654f4fe3aeef1f538
Kevin Wong <kevinwong@pursuit.org>
2019-12-17 19:15:17 -0500

    Add html, css and js files

---
+++ giphy.js
@@ -0,0 +1,9 @@
+// WARMUP:
+// 1. I want you to create a directory(folder) with a JavaScript file, a HTML file, and CSS file.
+// 2. The <body> tag should contain a <form> tag, and a <div>tag with a class of “content”
+// 3. the <form> tag should contain an <input> tag with type=text, a select tag with a disabled <option> tag and another input tag with the type=submit
+// 2. Create an account on giphy https://developers.giphy.com/?gclid=EAIaIQobChMI9LC-1eu95gIVDYiGCh0OPAnvEAAYASAAEgL0E_D_BwE
+// developers.giphy.comdevelopers.giphy.com
+// GIPHY Developers
+// The GIPHY Developer Portal is how you integrate the world's best GIF API into your business, app or project.
+
